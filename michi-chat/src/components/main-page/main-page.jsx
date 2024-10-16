866b96336663e177ec6b9d9dac0a09586fc1b6f7
JacobIsai <jacob.borjasort@uanl.edu.mx>
2024-10-15 22:40:32 -0600

    GroupChat

--- michi-chat/src/components/main-page/main-page.jsx
+++ michi-chat/src/components/main-page/main-page.jsx
@@ -7,88 +7,87 @@ import NewChat from '../new-chat/newChat'
 import NewGroupChat from '../new-group-chat/newGroupChat'
 import ChatBox from '../chat-box/chatBox'
 
-
 import SearchIcon from '@mui/icons-material/Search';
 import { TextField, InputAdornment } from '@mui/material';
 import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
-
 
 import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore'
 import { db } from '../../lib/firebase'
 import { useUserStore } from '../../lib/userStore'
 import { useChatStore } from '../../lib/chatStore'
 
-
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
-
 function mainPage() {
-  //new chat modal
   const [modal, setModal] = useState(false);
   const [groupModal, setGroupModal] = useState(false);
 
-  const toggleModal = ()=>{
-    setModal(!modal)
+  const toggleModal = () => {
+    setModal(!modal);
     handleClose();
-  }
+  };
 
   const toggleGroupModal = () => {
     setGroupModal(!groupModal);
     handleClose();
   };
-  //chats
+
   const [chats, setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
-  const {currentUser} = useUserStore();
-  const {changeChat} = useChatStore();
-  const {chatId} = useChatStore();
+  const { currentUser } = useUserStore();
+  const { changeChat, chatId } = useChatStore();
 
-  useEffect(()=>{
-    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res)=>{
+  // Fetch both personal and group chats
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
       const items = res.data().chats;
 
-      const promises = items.map(async (item)=>{
-        const userDocRef = doc(db, "users", item.receiverId);
-        const userDocSnap = getDoc(userDocRef);
-
-        const user = (await userDocSnap).data();
-        return{...item, user};
+      const promises = items.map(async (item) => {
+        if (item.isGroupChat) {
+          return {
+            ...item,
+            groupName: item.groupName || 'Chat grupal',
+            groupMembers: item.members || [],
+          };
+        } else {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = getDoc(userDocRef);
+          const user = (await userDocSnap).data();
+          return { ...item, user };
+        }
       });
+
       const chatData = await Promise.all(promises);
-      setChats(chatData.sort((a,b)=>b.updatedAt - a.updatedAt));
-
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
     });
-    return ()=>{
+
+    return () => {
       unSub();
-    }
-  }, [currentUser.id])
+    };
+  }, [currentUser.id]);
 
-  const handleSelect = async (chat) => { // Verificar si chatId y user están presentes
-   
-    const userChats = chats.map((item) => {
-      const { user, ...rest } = item;
-      return rest;
-    });
-
-    const chatIndex = userChats.findIndex(
-      (item) => item.chatId === chat.chatId
-    );
-
-    userChats[chatIndex].isSeen = true;
+  const handleSelect = async (chat) => {
+    const chatIndex = chats.findIndex((item) => item.chatId === chat.chatId);
+    chats[chatIndex].isSeen = true;
 
     const userChatsRef = doc(db, "userchats", currentUser.id);
 
     try {
       await updateDoc(userChatsRef, {
-        chats: userChats,
+        chats: chats,
       });
-      changeChat(chat.chatId, chat.user);
+
+      if (chat.isGroupChat) {
+        changeChat(chat.chatId, { groupName: chat.groupName, members: chat.groupMembers, blocked: [] });
+      } else {
+        changeChat(chat.chatId, chat.user);
+      }
     } catch (err) {
       console.log(err);
     }
   };
-  ////////////DROPDOWN MENU//////////
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -98,68 +97,86 @@ function mainPage() {
     setAnchorEl(null);
   };
 
-
   return (
     <div className='mainpage'>
-      <div>
-        <Header></Header>
-      </div>
+      <Header />
       <div className={`login ${modal || groupModal ? 'blur-background' : ''} main`}>
-        
-      <div className="chat_list">
-        <div className="search">
-          <button 
-          id="chat-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
-          ><AddToPhotosIcon sx={{ fontSize: 40 }}></AddToPhotosIcon></button>
-        <TextField
-          placeholder="Search..."
-          className="custom-input"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon sx={{ fontSize: 40 }}/>
-              </InputAdornment>
-            ),
-          }}
-          variant="outlined"
-        />
-        </div>
-        {chats.map((chat)=>(
-          <div onClick={() => handleSelect(chat)} key={chat.chatId}>
-            <ChatBox
-            chatName={chat.user.username} 
-            lastMessage={chat.lastMessage}
-            seen={chat.isSeen}
-            time={new Date(chat.updatedAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-            chatPicture={chat.user.avatar}/>
+        <div className="chat_list">
+          <div className="search">
+            <button 
+              id="chat-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <AddToPhotosIcon sx={{ fontSize: 40 }} />
+            </button>
+            <TextField
+              placeholder="Search..."
+              className="custom-input"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon sx={{ fontSize: 40 }}/>
+                  </InputAdornment>
+                ),
+              }}
+              variant="outlined"
+            />
           </div>
-          
-        ))}
 
-      </div>
+          {/* Render chat list */}
+          {chats.map((chat) => (
+            <div onClick={() => handleSelect(chat)} key={chat.chatId}>
+              {chat.isGroupChat ? (
+                <ChatBox
+                  chatName={chat.groupName}
+                  lastMessage={chat.lastMessage}
+                  seen={chat.isSeen}
+                  time={new Date(chat.updatedAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                  chatPicture={'/path-to-group-avatar.png'} // Default group avatar
+                />
+              ) : (
+                chat.user ? (
+                  <ChatBox
+                    chatName={chat.user.username}
+                    lastMessage={chat.lastMessage}
+                    seen={chat.isSeen}
+                    time={new Date(chat.updatedAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                    chatPicture={chat.user.avatar}
+                  />
+                ) : (
+                  <div>No se pudo cargar la información del usuario</div>
+                )
+              )}
+            </div>
+          ))}
+        </div>
 
         <div className="chat_space">
-          {chatId && <Chat></Chat>}
+        {chatId && <Chat chatId={chatId} />}
         </div>
       </div>
+
       {modal && (
         <div className="modal">
-              <div className="overlay">
-                <NewChat closeModal={toggleModal}></NewChat>
-              </div>
+          <div className="overlay">
+            <NewChat closeModal={toggleModal} />
+          </div>
         </div>
       )}
+
       {groupModal && (
         <div className="modal">
-              <div className="overlay">
-                <NewGroupChat closeModal={toggleGroupModal}></NewGroupChat>
-              </div>
+          <div className="overlay">
+            <NewGroupChat 
+              closeModal={toggleGroupModal}
+            />
+          </div>
         </div>
       )}
+
       <Menu
         className='dropdown-menu'
         id="chat-menu"
@@ -174,8 +191,7 @@ function mainPage() {
         <MenuItem onClick={toggleGroupModal}>Chat grupal nuevo</MenuItem>
       </Menu>
     </div>
-
-  )
+  );
 }
 
-export default mainPage
+export default mainPage;
--- michi-chat/src/components/new-group-chat/newGroupChat.jsx
+++ michi-chat/src/components/new-group-chat/newGroupChat.jsx
@@ -1,16 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './newGroupChat.css';
 import CloseIcon from '@mui/icons-material/Close';
 import { TextField, InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
+import { collection, getDocs, doc, setDoc, updateDoc, serverTimestamp, arrayUnion } from 'firebase/firestore';
+import { db } from '../../lib/firebase';
+import { useUserStore } from '../../lib/userStore';
 
-function newGroupChat({ isOpen, closeModal, children }) {
+function newGroupChat({ isOpen, closeModal }) {
+  const [users, setUsers] = useState([]); 
+  const [selectedUsers, setSelectedUsers] = useState([]); 
+  const { currentUser } = useUserStore(); 
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const userCollection = collection(db, 'users'); 
+      const userSnapshot = await getDocs(userCollection);
+      const userList = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setUsers(userList);
+    };
+
+    fetchUsers();
+  }, []);
+
+  
+  const handleUserSelect = (user) => {
+    if (selectedUsers.length < 5 && !selectedUsers.includes(user)) {
+      setSelectedUsers([...selectedUsers, user]);
+    }
+  };
+
+  
+  const handleUserRemove = (userToRemove) => {
+    setSelectedUsers(selectedUsers.filter(user => user !== userToRemove));
+  };
+
+  
+  const handleCreateGroupChat = async () => {
+    if (selectedUsers.length >= 3) {
+      try {
+        
+        const chatRef = collection(db, 'chats');
+        const newChatRef = doc(chatRef); 
+
+        
+        await setDoc(newChatRef, {
+          createdAt: serverTimestamp(),
+          members: selectedUsers.map(user => user.id).concat(currentUser.id), // Incluir al usuario actual
+          isGroupChat: true,
+          messages: []
+        });
+
+       
+        await updateDoc(doc(db, 'userchats', currentUser.id), {
+          chats: arrayUnion({
+            chatId: newChatRef.id,
+            lastMessage: '',
+            isGroupChat: true,
+            updatedAt: Date.now()
+          })
+        });
+
+        
+        for (const user of selectedUsers) {
+          await updateDoc(doc(db, 'userchats', user.id), {
+            chats: arrayUnion({
+              chatId: newChatRef.id,
+              lastMessage: '',
+              isGroupChat: true,
+              updatedAt: Date.now()
+            })
+          });
+        }
+
+        closeModal(); 
+      } catch (err) {
+        console.error('Error al crear el grupo de chat:', err);
+      }
+    }
+  };
+
   return (
     <div className='new-group-chat'>
       <div className='new-chat-search'>
         <TextField
-          
           placeholder='Search...'
           className='custom-input'
           InputProps={{
@@ -28,60 +102,38 @@ function newGroupChat({ isOpen, closeModal, children }) {
           </button>
         </div>
       </div>
+
       <div className='new-groupchat-list'>
-        <div className='new-groupchat-item'>
-          <img src={'src/assets/pictures/avatar-blank.png'} alt='' />
-          <h2>Username</h2>
-          <button className='btn'><AddIcon></AddIcon></button>
-        </div>
-        <div className='new-groupchat-item'>
-          <img src={'src/assets/pictures/avatar-blank.png'} alt='' />
-          <h2>Username</h2>
-          <button className='btn'><AddIcon></AddIcon></button>
-        </div>
-        <div className='new-groupchat-item'>
-          <img src={'src/assets/pictures/avatar-blank.png'} alt='' />
-          <h2>Username</h2>
-          <button className='btn'><AddIcon></AddIcon></button>
-        </div>
-        <div className='new-groupchat-item'>
-          <img src={'src/assets/pictures/avatar-blank.png'} alt='' />
-          <h2>Username</h2>
-          <button className='btn'><AddIcon></AddIcon></button>
-        </div>
-        <div className='new-groupchat-item'>
-          <img src={'src/assets/pictures/avatar-blank.png'} alt='' />
-          <h2>Username</h2>
-          <button className='btn'><AddIcon></AddIcon></button>
-        </div>
+        {users.map((user) => (
+          <div key={user.id} className='new-groupchat-item'>
+            <img 
+              src={user.avatar || 'src/assets/pictures/avatar-blank.png'} 
+              alt={`${user.username}'s avatar`} 
+              className='user-avatar' 
+            />
+            <h2>{user.username}</h2>
+            <button className='btn' onClick={() => handleUserSelect(user)}>
+              <AddIcon />
+            </button>
+          </div>
+        ))}
       </div>
+
       <div className="new-group-list">
         <div className="items">
-          <div className="gc-new-item">
-            <h4>Username1</h4>
-            <CloseIcon className='btn-cerrar'></CloseIcon>
-          </div>
-          <div className="gc-new-item">
-            <h4>Username1</h4>
-            <CloseIcon className='btn-cerrar'></CloseIcon>
-          </div>
-          <div className="gc-new-item">
-            <h4>Username1</h4>
-            <CloseIcon className='btn-cerrar'></CloseIcon>
-          </div>
-          <div className="gc-new-item">
-            <h4>Username1</h4>
-            <CloseIcon className='btn-cerrar'></CloseIcon>
-          </div>
-          <div className="gc-new-item">
-            <h4>Username1</h4>
-            <CloseIcon className='btn-cerrar'></CloseIcon>
-          </div>
+          {selectedUsers.map((user) => (
+            <div key={user.id} className="gc-new-item">
+              <h4>{user.username}</h4>
+              <CloseIcon className='btn-cerrar' onClick={() => handleUserRemove(user)} />
+            </div>
+          ))}
         </div>
-          <button className='btn'>Crear groupchat</button>
+        <button className='btn' disabled={selectedUsers.length < 3} onClick={handleCreateGroupChat}>
+          Crear groupchat
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default newGroupChat
+export default newGroupChat;
